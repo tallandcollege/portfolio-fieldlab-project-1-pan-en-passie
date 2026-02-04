45b26ac5e16f344f441b03c9a2fa759f61f3799f
joelWijn <157014542+joelWijn@users.noreply.github.com>
2026-02-04 20:04:24 +0100

    Create Recepten.php

---
+++ Recepten.php
@@ -0,0 +1,31 @@
+<?php
+include "includes/connect.php";
+$pdo = connect();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php include("includes/header.php"); ?>
+        <?php
+        try{
+            echo "<tr>";
+            $receptenQuery= "SELECT * FROM recipe";
+            $recepten = $pdo->query($receptenQuery);
+            
+            foreach ($recepten as $rec) {
+                echo $rec['name'];
+            }
+        } catch (PDOException $e) {
+            echo "Connection Failed";
+        }
+        ?>
+    <?php include("Includes/footer.php"); ?>
+</body>
+</html>
