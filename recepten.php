ee62bd360445a62030bdb18459cd4c2f8b6cd393
LennardSimon <174249@student.horizoncollege.nl>
2026-02-12 14:53:39 +0100

    Revert "Merge branch 'production' into ReceptenTonen"
    This reverts commit 47c476822d834b0dc66936b85f3e63a4d9ea07c4, reversing
    changes made to 7be699d7d2389c97cc819d487f29e7d0dcd5eedc.

---
+++ recepten.php
@@ -0,0 +1,74 @@
+<?php
+include "includes/connection.php";
+$pdo = connect();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zoekresultaten</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<?php include("includes/header.php"); ?>
+
+<section class="recepten-section">
+
+<?php
+$search = $_GET["search"] ?? ""; // voorkomt undefined error
+
+try {
+
+    echo "<table>";
+
+    if (!empty($search)) {
+        //zoekt naar ingevoerde info in name of description
+        $receptenQuery = "
+            SELECT * 
+            FROM recipe 
+            WHERE Name LIKE :search 
+            OR Description LIKE :search
+        ";
+
+        $stmt = $pdo->prepare($receptenQuery);
+        $stmt->execute([
+            ':search' => "%$search%"
+        ]);
+        //Checkt of het recept in de database zit
+        if($stmt->rowCount() > 0){
+        //print iedere bijpassende recept
+        foreach ($stmt as $rec) {
+            echo "<tr>";
+            echo "<td>";
+            echo "<div class='receptenstuk'>";
+            echo "<a href='recept_pagina.php?id=" . $rec['id'] . "'>";
+            echo "<h2>" . htmlspecialchars($rec['Name']) . "</h2>";
+            echo "<div class='receptendetails'>" . htmlspecialchars($rec['Description']) . "</div>";
+            echo "<h6 class='receptentijd'>" . $rec['Createdat'] . "</h6>";
+            echo "</a>";
+            echo "</div>";
+            echo "</td>";
+            echo "</tr>";
+        }
+        } else {
+            echo "Geen bijpassende recept gevonden.";
+        }
+    } else {
+        echo "<tr><td>Geen zoekterm ingevoerd.</td></tr>";
+    }
+
+    echo "</table>";
+
+} catch (PDOException $e) {
+    echo "Database fout: " . $e->getMessage();
+}
+?>
+
+</section>
+
+<?php include("includes/footer.php"); ?>
+</body>
+</html>
